db4e9527bee46dd51ea75e6e5f59c0464fe9fe35
Zerattel <112253325+Zerattel@users.noreply.github.com>
2025-07-22 15:21:31 +0300

    zerattel | speed nerf, neosteel nerf, new resistors
    * Speed nerf, neosteel nerf, new resistors
    
    ---------
    
    Co-authored-by: Makar Ts <tsivikov.makar@gmail.com>

--- libs/hayat/battleships.js
+++ libs/hayat/battleships.js
@@ -20,7 +20,7 @@ const tonnageToCaptureRange = [
 
 const tonnageToManeuverabilityBonus = [5, 4, 3, 2, 1, 0, -2, -4, -6, -10];
 
-const tonnageToAcceleration = [900, 600, 420, 300, 180, 120, 90, 60, 30, 18];
+const tonnageToAcceleration = [240, 180, 150, 120, 90, 72, 54, 36, 18, 12];
 
 const energyToKineticDamage = 0.0000017;
 const energyToHeating = 0.000000001;
